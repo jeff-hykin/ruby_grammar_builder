cbe2901617d0f1d5ba4a8b6e747ae995f1bc8f21
Jeff Hykin <jeff.hykin@gmail.com>
2021-09-09 09:56:16 -0500

    fix require change

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source "http://rubygems.org"
 gem "gem-release"
-gem "ruby_grammar_builder", "0.0.4"
+# gem "ruby_grammar_builder", "0.0.5"
--- gemset.nix
+++ gemset.nix
@@ -1,14 +1,4 @@
 {
-  deep_clone = {
-    groups = ["default"];
-    platforms = [];
-    source = {
-      remotes = ["http://rubygems.org"];
-      sha256 = "003iqvmxxcm7p6qr2aafi1p5djm6ycvwzrjk6shc9wvybkvbckmz";
-      type = "gem";
-    };
-    version = "0.0.1";
-  };
   gem-release = {
     groups = ["default"];
     platforms = [];
@@ -18,16 +8,5 @@
       type = "gem";
     };
     version = "2.2.2";
-  };
-  ruby_grammar_builder = {
-    dependencies = ["deep_clone"];
-    groups = ["default"];
-    platforms = [];
-    source = {
-      remotes = ["http://rubygems.org"];
-      sha256 = "1m1ynjjcv7kf8414qnq5am3k6f1kajc76gbrwrxyhyc31j0n7sag";
-      type = "gem";
-    };
-    version = "0.0.4";
   };
 }
